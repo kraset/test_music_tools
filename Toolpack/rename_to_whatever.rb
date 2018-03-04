ace2b55dc5ab18bba901fea59cd9de86a9c6453c
Christian Andersson <kraset74@yahoo.se>
2018-03-04 23:03:46 +0100

    Added first file.

---
+++ Toolpack/rename_to_whatever.rb
@@ -0,0 +1,19 @@
+# Testing git first commit.
+
+def rename_to_whatever(extension, from_pattern, to_pattern)
+    Dir.glob("*."+extension).each do |file_name|
+        File.rename(file_name, file_name.gsub(from_pattern, to_pattern))
+    end
+
+end
+
+puts "Don't forget to edit this file with (extension, from_pattern, to_pattern) before running it!"
+# Wait keypress
+option = gets.chomp 
+
+
+rename_to_whatever(
+	"wav", #file extension, e.g. wav/mp3/cpr
+	"Discovering_", #from pattern
+	"In_", #to pattern
+)
